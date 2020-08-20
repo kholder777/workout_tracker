b3ce0fb67cde1fa9717a61526332ec9654ad2499
Kevin Holder <kholder777@yahoo.com>
2020-08-20 16:26:16 -0500

    Removed Develop folder after exporting all files.

---
+++ models/workout.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//schema: exercises fields
+const ExerciseSchema = new Schema({
+	type: { type: String, trim: true },
+	name: { type: String, trim: true },
+	duration: { type: Number, trim: true },
+	weight: { type: Number, trim: true },
+	reps: { type: Number, trim: true },
+	sets: { type: Number, trim: true },
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
