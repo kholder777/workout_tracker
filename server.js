7f927581d0a2ae5945ebbf1a8f745aa0933b4f6f
Kevin Holder <kholder777@yahoo.com>
2020-08-21 10:36:59 -0500

    Added html routes

--- server.js
+++ server.js
@@ -15,7 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-//TODO: install html routes.
+// html routes.
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
 
 // TODO: modify code below for api routes.
 //connects to workout db/ deployment process.
